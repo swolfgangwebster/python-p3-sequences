26a92fe3b957bc0a2d98d7face03f9792cd6717d
Shawn Webster <swolfgangwebster@gmail.com>
2023-07-15 19:10:59 -0500

    fib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    a = 0
+    b = 1
+    fib = [a,b]
+
+    if length == 0:
+        print([])
+        return
+    if length == 1:
+        print([0])
+        return
+
+    while (length-2 > 0):
+        length = length - 1
+        fib = fib + [fib[-2] + fib[-1]]
+    
+    print(fib)
     pass
